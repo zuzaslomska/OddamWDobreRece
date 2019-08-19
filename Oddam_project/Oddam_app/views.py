14f3ce55b0d786561552ee1fbece7dd45d31fa41
zuzaslomska <zuzaslomska@gmail.com>
2019-08-19 22:43:55 +0200

    working login

--- Oddam_project/Oddam_app/views.py
+++ Oddam_project/Oddam_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.urls import reverse_lazy
 from django.views.generic import (FormView, CreateView, RedirectView, View, DetailView, ListView, UpdateView, TemplateView)
-from Oddam_app.forms import RegistrationForm
+from Oddam_app.forms import RegistrationForm, LoginForm
 
 
 class LandingPage(TemplateView):
@@ -9,12 +11,27 @@ class LandingPage(TemplateView):
 class Registration(FormView):
     template_name = 'register.html'
     form_class = RegistrationForm
-    success_url = '/login/'
+    success_url = reverse_lazy('login')
 
     def form_valid(self, form):
         form.save()
 
         return super(Registration,self).form_valid(form)
 
-class Login(TemplateView):
+
+class Login(FormView):
     template_name = 'login.html'
+    form_class = LoginForm
+    success_url = reverse_lazy('giveaway')
+
+    def form_valid(self, form):
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return super().form_valid(form)
+
+
+class GiveawayView(TemplateView):
+    template_name = 'form.html'
